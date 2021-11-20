b206b6bb041f860316c8656e316f0a624ca37257
MORIKOOH <ruralsouladvance@gmail.com>
2021-11-20 15:36:59 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 'use strict';
+
+// メモ化再帰と呼ばれる処理？
+// 結果を格納する連想配列
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
+// フィボナッチ数を計算・格納・返す関数
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    // Map#memo が key: n を持っていれば(n < 2)、
+    // key:n に対応する value を返す
+    if (memo.has(n)) {
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    // Map#memo が key: n を持っていなければ(n >= 2)、
+    // 再帰関数を計算してフィボナッチ数を計算し、
+    // Map#memo に格納 => フィボナッチ数を返す
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
